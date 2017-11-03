727dc8308b7f50df48c0d59abbcab88caaacbecc
kiran <kiran.alef@gmail.com>
2017-11-03 20:01:07 +0000

    Parse content uri into file path

--- android/src/main/java/com/reactnativedocumentpicker/DocumentPicker.java
+++ android/src/main/java/com/reactnativedocumentpicker/DocumentPicker.java
@@ -1,12 +1,20 @@
 package com.reactnativedocumentpicker;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
 import android.provider.OpenableColumns;
+import android.support.annotation.NonNull;
 import android.util.Log;
 import android.webkit.MimeTypeMap;
 
@@ -23,6 +31,7 @@ import com.facebook.react.bridge.WritableMap;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.URI;
 import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
@@ -74,23 +83,50 @@ public class DocumentPicker extends ReactContextBaseJavaModule implements Activi
         getReactApplicationContext().startActivityForResult(intent, READ_REQUEST_CODE, Bundle.EMPTY);
     }
 
-    // removed @Override temporarily just to get it working on RN0.33 and RN0.32 - will remove
+    @Override //temporarily just to get it working on RN0.33 and RN0.32 - will remove
     public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent data) {
         onActivityResult(requestCode, resultCode, data);
+    }
+    private WritableMap response = Arguments.createMap();
+
+    public void cleanResponse()
+    {
+        response = Arguments.createMap();
+    }
+
+    public void invokeCancel(@NonNull final Callback callback)
+    {
+        cleanResponse();
+        response.putBoolean("didCancel", true);
+        invokeResponse(callback);
+    }
+
+    public void invokeError(@NonNull final Callback callback, String reason)
+    {
+        cleanResponse();
+        response.putString("error", reason);
+        invokeResponse(callback);
+    }
+
+    public void invokeResponse(@NonNull final Callback callback)
+    {
+        callback.invoke(response,Arguments.createMap());
     }
 
     // removed @Override temporarily just to get it working on RN0.33 and RN0.32
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.d("TAG","onActivityResult: "+resultCode);
         if (requestCode != READ_REQUEST_CODE)
             return;
 
         if (resultCode != Activity.RESULT_OK) {
-            callback.invoke("Bad result code: " + resultCode, null);
+            invokeCancel(callback);
+            //callback.invoke("Bad result code: " + resultCode, null);
             return;
         }
 
         if (data == null) {
-            callback.invoke("No data", null);
+            invokeError(callback,"No data");
             return;
         }
 
@@ -99,9 +135,138 @@ public class DocumentPicker extends ReactContextBaseJavaModule implements Activi
             callback.invoke(null, toMapWithMetadata(uri));
         } catch (Exception e) {
             Log.e(NAME, "Failed to read", e);
-            callback.invoke(e.getMessage(), null);
+            invokeError(callback,e.getMessage());
         }
     }
+
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+    public static String getPath(final Context context, final Uri uri) {
+
+        final boolean isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
+        // DocumentProvider
+        if (isKitKat && DocumentsContract.isDocumentUri(context, uri)) {
+            // ExternalStorageProvider
+            if (isExternalStorageDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                if ("primary".equalsIgnoreCase(type)) {
+                    return Environment.getExternalStorageDirectory() + "/" + split[1];
+                }
+
+
+                // TODO handle non-primary volumes
+            }
+            // DownloadsProvider
+            else if (isDownloadsDocument(uri)) {
+
+                final String id = DocumentsContract.getDocumentId(uri);
+                final Uri contentUri = ContentUris.withAppendedId(
+                        Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
+
+                return getDataColumn(context, contentUri, null, null);
+            }
+            // MediaProvider
+            else if (isMediaDocument(uri)) {
+
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                Uri contentUri = null;
+                if ("image".equals(type)) {
+                    contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                } else if ("video".equals(type)) {
+                    contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                } else if ("audio".equals(type)) {
+                    contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                }
+
+                final String selection = "_id=?";
+                final String[] selectionArgs = new String[] {
+                        split[1]
+                };
+
+                return getDataColumn(context, contentUri, selection, selectionArgs);
+            }
+        }
+        // MediaStore (and general)
+        else if ("content".equalsIgnoreCase(uri.getScheme())) {
+            Log.d("TAG","getPath: MediaStore");
+
+            // Return the remote address
+            if (isGooglePhotosUri(uri))
+                return uri.getLastPathSegment();
+
+            return getDataColumn(context, uri, null, null);
+        }
+        // File
+        else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            return uri.getPath();
+        }
+
+        return null;
+    }
+
+    public static String getDataColumn(Context context, Uri uri, String selection,
+                                       String[] selectionArgs) {
+
+        Cursor cursor = null;
+        final String column = "_data";
+        final String[] projection = {
+                column
+        };
+
+        try {
+            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs,
+                    null);
+            if (cursor != null && cursor.moveToFirst()) {
+                final int index = cursor.getColumnIndexOrThrow(column);
+                return cursor.getString(index);
+            }
+        } finally {
+            if (cursor != null)
+                cursor.close();
+        }
+        return null;
+    }
+
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is ExternalStorageProvider.
+     */
+    public static boolean isExternalStorageDocument(Uri uri) {
+        return "com.android.externalstorage.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is DownloadsProvider.
+     */
+    public static boolean isDownloadsDocument(Uri uri) {
+        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is MediaProvider.
+     */
+    public static boolean isMediaDocument(Uri uri) {
+        return "com.android.providers.media.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is Google Photos.
+     */
+    public static boolean isGooglePhotosUri(Uri uri) {
+        return "com.google.android.apps.photos.content".equals(uri.getAuthority());
+    }
+
+
+
 
     private WritableMap toMapWithMetadata(Uri uri) {
         WritableMap map;
@@ -114,6 +279,11 @@ public class DocumentPicker extends ReactContextBaseJavaModule implements Activi
         }
 
         map.putString("uri", uri.toString());
+
+        String path = getPath(getReactApplicationContext(),uri);
+        map.putString("filePath", path);
+
+
 
         return map;
     }
